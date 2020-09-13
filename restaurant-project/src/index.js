298ca40697fb5200940e38bfceb1b45259e1e946
Jonathan Kim <jonkim1121@gmail.com>
2020-09-13 12:37:25 -0500

    First Commit, added src code and README

---
+++ restaurant-project/src/index.js
@@ -0,0 +1,7 @@
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    alert("hello");
+  }
+  
